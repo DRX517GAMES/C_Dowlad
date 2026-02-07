900ca482178e9f64ee47037219d19f3b2b5636ad
DRX517GAMES <dawid.profus@interia.pl>
2026-02-07 14:49:17 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,10 @@
 const cacheName = 'c-v1';
-// Lista plików do zapisania w pamięci urządzenia
-const assets = [
-  './',
-  'index.html',
-  'manifest.json',
-  'https://www.soundjay.com/mechanical/sounds/explosion-01.mp3'
-];
+const assets = ['./', 'index.html', 'gra.html', 'manifest.json'];
 
-// Instalacja Service Workera i pobieranie plików
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('C: Buforowanie plików gry...');
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Obsługa gry w trybie offline
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
